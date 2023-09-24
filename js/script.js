a1a6a6cb2c2e1a6361a227d38243b2d93c0c60e2
maxg40 <maxigossa@gmail.com>
2023-09-24 14:24:18 -0300

    acomodar codigo para subir a web

--- js/script.js
+++ js/script.js
@@ -3,4 +3,5 @@ Swal.fire({
     text: '30% DE DESCUENTO en transferencia o efectivo',
     imageUrl: 'https://github.com/maxg40/exoticaspop/blob/main/img/LOGO2.png?raw=true',
     imageAlt: 'logo exoticas',
+    backdrop:'rgba(0,0,1,0.9)',
 })
