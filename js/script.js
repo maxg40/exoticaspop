bf70ebbfd4498607d25bc6a3f386c31702e3ac2f
maxg40 <maxigossa@gmail.com>
2023-09-24 12:03:09 -0300

    a

---
+++ js/script.js
@@ -0,0 +1,8 @@
+Swal.fire({
+    title: 'Sweet!',
+    text: 'Modal with a custom image.',
+    imageUrl: './img/LOGO2.png',
+    imageWidth: 200,
+    imageHeight: 200,
+    imageAlt: 'Custom image',
+})
