e9baa94846beba520039efb4f1710a3927fa6ecf
maxg40 <maxigossa@gmail.com>
2023-09-24 14:43:51 -0300

    cambio de color

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 Swal.fire({
-    title: 'BIENVENIDOS',
-    text: '30% DE DESCUENTO en transferencia o efectivo',
+    title: 'BIENVENID@',
+    text: '30% de DESCUENTO efectivo o transferencia',
     imageUrl: 'https://github.com/maxg40/exoticaspop/blob/main/img/LOGO2.png?raw=true',
-    imageAlt: 'logo exoticas',
     backdrop:'rgba(0,0,1,0.9)',
+    background: '#ffc0cb',
 })
