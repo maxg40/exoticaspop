b9a68d252409e8d4ba2579a1ffee69b1e7bc8828
maxg40 <maxigossa@gmail.com>
2023-10-10 15:26:17 -0300

    cambio descuento 15%

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 Swal.fire({
     title: 'BIENVENID@',
-    text: '30% de DESCUENTO efectivo o transferencia',
+    text: '15% de DESCUENTO efectivo o transferencia',
     imageUrl: 'https://github.com/maxg40/exoticaspop/blob/main/img/LOGO2.png?raw=true',
     backdrop:'rgba(0,0,1,0.9)',
     background: '#ffc0cb',
